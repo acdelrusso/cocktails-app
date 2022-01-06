0b2d1eda42c3cbc3cae59c97aabe4510707f8aa1
acdelrusso <88458576+acdelrusso@users.noreply.github.com>
2022-01-06 14:48:21 -0500

    commit of finished project

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iusto error eius in, quidem ipsum voluptatibus numquam amet iste iure nam tempora aliquam dolores vero minus, earum debitis, blanditiis vitae voluptas eaque soluta natus eveniet molestias. Sint optio nulla et!</p>
+    </section>
+  )
+}
+
+export default About
